ed13d14311640093a6f51fe7e81865b4dcfc780a
dhana <dhanab467@gmail.com>
2022-01-08 18:51:51 +0530

    commit

--- src/com/company/numberprograms.java
+++ src/com/company/numberprograms.java
@@ -9,7 +9,7 @@ public class numberprograms {
         String[] a=str.split(" ");
         for(int i=0;i<a.length;i++)
         {
-            System.out.print(a[i]+" ");
+            System.out.print(a[i]+" ") ;
         }
         System.out.println();
         for(int i=a.length-1;i>=0;i--)
