73d004668ae9c56a1d0fd1098b2c152764a8ea03
dhana <dhanab467@gmail.com>
2022-01-08 18:52:57 +0530

    commit

--- src/com/company/stringprograms.java
+++ src/com/company/stringprograms.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Locale;
 import java.util.Scanner;
@@ -179,10 +180,19 @@ public class stringprograms {
 
         /*String str="My Name is Dhana";
         String[] a=str.split(" ");
+        System.out.println("before reversing the string");
+        for(int i=0;i<a.length;i++)
+        {
+            System.out.print(a[i]+" ");
+        }
+        System.out.println();
+        System.out.println("after reversing the string");
         for(int i=a.length-1;i>=0;i--)
         {
             System.out.print(a[i]+" ");
         }*/
+
+
 
 //check whether string is palindrome or not
 
@@ -277,6 +287,214 @@ public class stringprograms {
         s1=s1.substring(s2.length());//store initial s1 to s2(lakshmi)
         System.out.println("swaping of s1 and s2 is :"+s1+ " " +s2);*/
 
+//split of string
+
+     /* String s="My=name=is=dhana";
+      String[] s2=s.split("=");
+        System.out.println(s2[0]);*/
+
+//caps of string
+
+     /* String s="i Am dHana";
+      String[] s2=s.split(" ");
+        //for(String w:s2)
+       *//* for(int i=0;i<s2.length;i++)
+      {
+          String first=s2[i].substring(0,1); //extract 1st letter in string-->will not extract 0th letter
+          String last =s2[i].substring(1);//extract letters from 1 to end-->will not extract 0th letter
+          String caps=first.toUpperCase()+last.toLowerCase();
+          System.out.println(caps);
+      }*/
+
+//converting char array to string
+
+     /*char ch[]={'s','t','r','i','n','g'};
+     String s=new String(ch);
+     System.out.println(s);*/
+
+//converting string to char array
+
+    /*String s="dhana";
+    char ch[]=s.toCharArray();
+        System.out.println(ch[0]+","+ch[1]);*/
+
+//compare two strings with equals and compare keywords
+
+   /* String s1="dhanad45";
+    String s2="dhana";
+    System.out.println(s1.equalsIgnoreCase(s2));
+        System.out.println(s1.compareTo(s2));*/
+
+//get substring
+
+    /*String s="dhana";
+    //String s1=s.substring(0,3);//dha(will get 0,1,2 positions)
+        String s1=s.substring(3);//na(will get from 3rd letter)
+        System.out.println(s1);*/
+
+//trim the string--eliminates the white spaces before and after the string
+
+   /* String tr=" dhana - lakshmi ";
+        System.out.println("before trim"+tr);
+        String tr1=tr.trim();
+        System.out.println("after trim"+tr1);//"dhana - lakshmi"(trimmed before and after the white space)
+        //in order to trim space for each string, first need to split and then trim the word
+        String[] s=tr1.split("-");
+        *//*String strim1=s[0].trim();//"dhana"
+        System.out.println("first word trim"+strim1);
+        String strim2=s[1].trim();//"lakshmi"
+        System.out.println("second word trim"+strim2);*//*
+        for(String w:s)
+        {
+            System.out.println(w.trim());
+        }*/
+
+//keep alphabets and spaces
+
+    /* String s="dhana @#90_+!lak^6";
+        System.out.println(s.replaceAll("[^a-zA-Z ]",""));*/
+
+//keep alphabets,spaces and numbers
+       /* String s1="dhana 5412 @#90_+!lak^6";
+        System.out.println(s.replaceAll("[^a-zA-Z0-9 ]",""));*/
+
+//remove numbers in string
+        /*String s2="dhana 5412 @#90_+!lak^6";
+        System.out.println(s.replaceAll("[^0-9]",""));*/
+
+//keep special charecters in string
+       /* String s3="dhana 5412 @#90_+!lak^6";
+        System.out.println(s.replaceAll("[0-9a-zA-Z ]",""));
+*/
+//convert string to int
+       /* String ss="125";
+        int a=Integer.parseInt(ss);
+        System.out.println(a+1);*/
+
+//convert int to string
+         /*int n=125;
+         String ss1=String.valueOf(n);
+        System.out.println(ss1+1);
+*/
+//compare '==' operator and '.equals' method
+
+        /*String str1=new String("dhana");
+         String str2=new String("dhana");
+         System.out.println(str1==str2);//use == operator for reference comparision
+         System.out.println(str1.equals(str2));//use equals operator for conten comparision
+        //use == operator for reference comparision
+        String str3="dhana";
+        String str4="dhana";
+        System.out.println(str3.equals(str4));*/
+
+//Remove consecutive letters in array
+
+        /*String str="dhhanaaa";
+        char cha[]=str.toCharArray();
+        char p='\0';
+        int k=0;
+       for(int i=cha[0];i<=cha.length;cha[i]++)
+        {
+            if(p!=cha[i])
+            {
+              cha[k++]=cha[i];
+              p=cha[i];
+
+            }
+        }
+        System.out.println(.substring(0,k));*/
+
+        //Remove consecutive letters in array
+
+       /*String str11="dhhanaaa";
+        String res="";
+        for(int i=0;i<str11.length();i++)
+        {
+            if(i<str11.length()-1 && str11.charAt(i)==str11.charAt(i+1))
+            {
+                continue;
+            }
+            else
+            {
+                //System.out.println(s.charAt(i));
+               res=res+str11.charAt(i);
+            }
+        }
+        System.out.println(res);*/
+
+        //remove consecutive numbers in array list
+
+        /*int arr[]={1,1,1,2,2,1,3,4,4,3};
+        for(int i=0;i<arr.length;i++)
+        {
+            if(i<arr.length-1 && arr[i]==arr[i+1])
+            {
+                continue;
+            }
+            else
+            {
+                System.out.println(arr[i]);
+            }
+        }*/
+
+        //contains method--->it will returns boolean
+
+       /* String s1="dhana";
+        String s2="lakshmidha";
+        System.out.println(s1.contains("dha"));*/
+
+//join the strings using join method
+
+   /*   String s1="dhana";
+      String s2="lakshmi";
+        System.out.println(String.join("dhanu",s1,s2));
+        System.out.println(String.join("",s1,s2));
+        System.out.println(String.join(" ",s1,s2));
+        System.out.println(String.join("-",s1,s2));*/
+
+//replace specific charecter or string
+
+      /*String s1="dhana lucky";
+        System.out.println(s1.replace("lucky","Lakshmi"));//replace old string new string
+        System.out.println(s1.replace("n","x"));//replace old charecter to new charecter*/
+
+//finding index using index method
+
+     /* String s="dhana";
+        System.out.println(s.indexOf('a'));*/
+
+//concatinate two strings
+
+   /* String s1="dhana";
+    String s2="lakshmi";
+        System.out.println(s1.concat(" lakshmi"));
+        System.out.println(s1+" lakshmi");*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -292,4 +510,5 @@ public class stringprograms {
 
 
     }
+
 }
